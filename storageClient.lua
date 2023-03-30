c8de1a7ce7f205b472a4779105ac8492416f2836
schindlershadow <schindlershadow@gmail.com>
2023-03-29 23:10:20 -0400

    only discover crafting servers if crafting is enabled

--- storageClient.lua
+++ storageClient.lua
@@ -2001,7 +2001,7 @@ if settings.get("StorageServer") == "StorageServer" or settings.get("StorageServ
     discoverServers("StorageServer")
 end
 
-if settings.get("CraftingServer") == "CraftingServer" or settings.get("CraftingServer") == nil then
+if settings.get("CraftingServer") == "CraftingServer" and settings.get("CraftingServer") == nil then
     discoverServers("CraftingServer")
 end
 
